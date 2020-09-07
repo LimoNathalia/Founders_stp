691603e2c0dc277433fda4ad147810196c09e290
Nathalia Limo <nathalialimo1995@gmail.com>
2020-09-07 11:00:11 +0200

    Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("responsive_nav").style.width = "350px";
+}
+
+function closeNav() {
+    document.getElementById("responsive_nav").style.width = "0";
+}
